9c2828f8165a182e616febdb3af8bbe5402a99e2
rriverod <rriverod@chicagobooth.edu>
2025-03-06 06:42:53 +0000

    removed positon from teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,6 @@
 #
 #  id                :bigint           not null, primary key
 #  number_of_players :integer
-#  position          :integer
 #  team_name         :string
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_06_054805) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_064215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_06_054805) do
   create_table "teams", force: :cascade do |t|
     t.string "team_name"
     t.integer "number_of_players"
-    t.integer "position"
     t.integer "league_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
