bca34a89d76bf474b4e2af8f4aa1e9f1be6f4cfc
rriverod <rriverod@chicagobooth.edu>
2025-03-07 22:37:18 +0000

    edit matches wip

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -33,7 +33,9 @@ class PlayersController < ApplicationController
     this_team_name = params.fetch("query_team_name")
     team= Team.where({:team_name => this_team_name}).at(0)
     if team==nil
-    the_player.team_id = Team.all.sample.id
+      flash[:alert] = "Team '#{this_league_name}' not found."
+      redirect_to("/players") and return
+
     else
     the_player.team_id = team.id
     end
@@ -53,13 +55,22 @@ class PlayersController < ApplicationController
     the_id = params.fetch("path_id")
     the_player = Player.where({ :id => the_id }).at(0)
 
-    the_player.player_id = params.fetch("query_player_id")
     the_player.first_name = params.fetch("query_first_name")
     the_player.last_name = params.fetch("query_last_name")
     the_player.position = params.fetch("query_position")
     the_player.jersey_number = params.fetch("query_jersey_number")
     the_player.dob = params.fetch("query_dob")
-    the_player.team_id = params.fetch("query_team_id")
+
+    this_team_name = params.fetch("query_team_name")
+    team= Team.where({:team_name => this_team_name}).at(0)
+    if team==nil
+      flash[:alert] = "Team '#{this_team_name}' not found."
+      redirect_to("/players") and return
+
+    else
+    the_player.team_id = team.id
+    end
+
 
     if the_player.valid?
       the_player.save
